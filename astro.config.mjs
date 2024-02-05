c60a52d719962f184d01e702d1d155d9f8bca584
Arpad Gabor <hello@arpadgabor.com>
2024-02-05 09:57:49 +0200

    feat: add sst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
+import aws from 'astro-sst'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -7,8 +8,9 @@ import metaTags from 'astro-meta-tags'
 import robotsTxt from 'astro-robots-txt'
 import icon from 'astro-icon'
 
-// https://astro.build/config
 export default defineConfig({
+	output: 'server',
 	site: 'https://arpadgabor.com',
+	adapter: aws(),
 	integrations: [mdx(), sitemap(), tailwind(), metaTags(), robotsTxt(), icon()],
 })
